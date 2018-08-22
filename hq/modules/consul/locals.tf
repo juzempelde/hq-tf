41d7794a89e8afbc14baa6b51d0d81f85983d34d
Timo Reitz <godsboss@gmx.de>
2018-08-22 20:47:21 +0200

    Bind and advertise correctly.

--- hq/modules/consul/locals.tf
+++ hq/modules/consul/locals.tf
@@ -4,6 +4,8 @@ locals {
     "${var.role == "server" ? "-server" : ""}",
     "${var.role == "server" ? "-bootstrap-expect=${var.bootstrap_expect}" : ""}",
     "-datacenter=${var.datacenter}",
+    "-bind={{ GetPrivateIP }}",
+    "-advertise={{ GetPublicIP }}",
     "${var.retry_join_address == "" ? "" : "-retry-join=${var.retry_join_address}"}",
     "${var.role == "ui" ? "-ui" : ""}"
   ]
